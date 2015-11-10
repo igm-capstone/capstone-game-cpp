acf48b5bcc9d16f6877548fd8820f04f25506669
Lucas Vasconcelos <garrafote@gmail.com>
2015-11-10 00:33:55 -0500

    added tool for updating subtrees

---
+++ rig3d-update-subtrees.sh
@@ -0,0 +1,24 @@
+prefix="Rig3D$1"
+echo $prefix
+
+cat $prefix/.gitmodules | grep -B1 url | while read line
+do
+    linearr=($line)
+
+    case ${linearr[0]} in
+        path)
+            repopath=${linearr[2]}
+            ;;
+        url)
+            #git rm -r $prefix/$repopath
+            #git commit -m"Removed submodule $prefix/$repopath folder to add subtree"
+            git subtree pull --prefix $prefix/$repopath ${linearr[2]} master --squash
+
+            if [ -f "$prefix/$repopath/.gitmodules" ]
+            then
+                test.sh "/$repopath"
+            fi
+            ;;
+    esac
+
+done
