9811b8423da9d91a6e602812235887e8cb80b832
Gabriel Ortega <gabriel.ortega56@gmail.com>
2015-11-14 17:20:23 -0500

    Added Ray cast function that takes max Distance. Under the hood this uses a LineAABB test that behaves slightly differently than RayAABB.

--- proto-03-remix/RayCast.h
+++ proto-03-remix/RayCast.h
@@ -34,7 +34,49 @@ int RayCast(RayCastHit<Vector>* hitInfo, Ray<Vector> ray, AABB<Vector>* aabbs, i
 	}
 
 	float minIndex = 0;
-	// TODO! create magnitude2 function
+
+	float prevDistance = magnitudeSquared(hits.at(0).hit->origin - ray.origin);
+	for (int i = 1; i < hits.size(); i++)
+	{
+		float distance = magnitudeSquared(hits.at(i).hit->origin - ray.origin);
+		if (distance < prevDistance)
+		{
+			hits.at(i).distance = sqrt(distance);
+			minIndex = i;
+			prevDistance = distance;
+		}
+	}
+
+	*hitInfo = hits.at(minIndex);
+
+	return 1;
+}
+
+template <class Vector>
+int RayCast(RayCastHit<Vector>* hitInfo, Ray<Vector> ray, AABB<Vector>* aabbs, int count, float maxDistance)
+{
+	std::vector<RayCastHit<Vector>> hits;
+	hits.reserve(count);
+
+	Line<vec3f> line(ray.origin, ray.origin + (ray.normal * maxDistance));
+
+	for (int i = 0; i < count; i++)
+	{
+		Vector poi;
+		float t;
+		if (IntersectLineAABB(ray, aabbs[i], poi, t))
+		{
+			hits.push_back({ &aabbs[i], poi , 0.0f });	// Temp store zero for distance to save on calculations
+		}
+	}
+
+	if (hits.empty())
+	{
+		return 0;
+	}
+
+	float minIndex = 0;
+
 	float prevDistance = magnitudeSquared(hits.at(0).hit->origin - ray.origin);
 	for (int i = 1; i < hits.size(); i++)
 	{
