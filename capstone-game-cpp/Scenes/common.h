d290b6a11465f0c4ce68b4eb3e4b60872d8ea748
Tiago Martines <txm7803@rit.edu>
2016-01-05 17:33:00 -0500

    New Level/Scene/App Structure. WIP.

---
+++ capstone-game-cpp/Scenes/common.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <Windows.h>
+#include <d3d11.h>
+#include "Rig3D\Engine.h"
+#include "Rig3D\Graphics\Interface\IScene.h"
+#include "Rig3D\Graphics\DirectX11\DX3D11Renderer.h"
+#include "Rig3D\Graphics\Interface\IMesh.h"
+#include "Rig3D\Common\Transform.h"
+#include "Memory\Memory\LinearAllocator.h"
+#include "Rig3D\Graphics\MeshLibrary.h"
