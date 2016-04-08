84a9e5041eac7955874281c92ebf1ae127595556
Gabriel Ortega <gabriel.ortega56@gmail.com>
2016-04-08 12:00:25 -0400

    Removed unused methods.

--- capstone-game-cpp/SceneObjects/Heal.h
+++ capstone-game-cpp/SceneObjects/Heal.h
@@ -13,8 +13,6 @@ public:
 	float mDuration;
 
 	void Spawn(vec3f pos, int UUID, float duration);
-	static void OnNetAuthorityChange(BaseSceneObject* obj, bool newAuth);
-	static void OnNetSyncTransform(BaseSceneObject* obj, vec3f newPos, quatf newRot);
 	static void OnTriggerEnter(BaseSceneObject* self, BaseSceneObject* other);
 
 private:
