dcf5794f8cfe739acb39083b01f35afc17972ca7
Tiago Martines <txm7803@rit.edu>
2016-05-06 13:12:41 -0400

    Font on main menu. Sprinter hair fix.

--- capstone-game-cpp/ScareTacticsApplication.cpp
+++ capstone-game-cpp/ScareTacticsApplication.cpp
@@ -72,7 +72,7 @@ ScareTacticsApplication::ScareTacticsApplication() :
 {
 	ImGuiIO& io = ImGui::GetIO();
 	io.Fonts->AddFontDefault();
-	io.Fonts->AddFontFromFileTTF("Assets/Wil2ghan.ttf", 30.0f, NULL, NULL);
+	io.Fonts->AddFontFromFileTTF("Assets/UI/Fonts/ashcanbb_reg.ttf", 30.0f, NULL, NULL);
 }
 
 ScareTacticsApplication::~ScareTacticsApplication()
